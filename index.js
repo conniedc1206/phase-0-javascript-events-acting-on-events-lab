b00c5a5ea411176a1030789e0ce5ef22d2264b04
Connie Park <conniedc1206@gmail.com>
2022-05-23 10:41:49 -0700

    commit tree f82097c9feb79e0a4db8ac34ce33aeedf866277f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+//grab the dodger
+const dodger = document.getElementById("dodger");
+
+//build our moveDodgerLeft() function, adding a check on the current position of the dodger
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+//let's wire this up to our event listener
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+});
+
+//build our moveDodgerRight() function, adding a check on the current position of the dodger
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (0 < left < 40) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+//let's wire this up to our event listener
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+});
